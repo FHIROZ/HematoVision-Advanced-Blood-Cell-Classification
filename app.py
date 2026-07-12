bdba1bcb553429b77f5bde58bfdc2125ee787d16
Fhiroz Khan Shaik <161280861+FHIROZ@users.noreply.github.com>
2026-07-12 06:12:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,5 +74,6 @@ def index():
                        class_probs=[])
 
 
-if __name__ == '__main__':  # Correct
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
